31efabc1cf3380c0c455ad058a3ee2cbd7fe5d04
root <root@localhost.localdomain>
2026-01-13 23:19:46 +0000

    updated video_stats.py

--- video_stats.py
+++ video_stats.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path="./.env")
 
 API_KEY=os.getenv("API_KEY")
-
+maxResults = 50
 CHANNEL="MrBeast"
 
 def get_playlist_id():
@@ -28,5 +28,36 @@ def get_playlist_id():
     except requests.exceptions.RequestExceptions as e:
         raise e
 
+def get_video_ids(playlistId):
+
+    video_ids = []
+    pageToken = None
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}"
+    try:
+        while True:
+            url = base_url
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+
+
+            response=requests.get(url)
+            response.raise_for_status()
+            data=response.json()
+            
+            for item in data.get("items",[]):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+            
+            pageToken = data.get('nextPageToken')
+
+            if not pageToken:
+                break
+
+            return video_ids
+
+    except requests.exceptions.RequestExceptions as e:
+        raise e
+
 if __name__ == "__main__":
-    get_playlist_id()
+    playlistId = get_playlist_id()
+    print(get_video_ids(playlistId))
