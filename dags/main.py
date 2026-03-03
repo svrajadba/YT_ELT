9c98fe2a537aa0be48d885315dd1c7108beac5b3
root <root@localhost.localdomain>
2026-03-03 22:57:35 +0000

    last_but_one

--- dags/main.py
+++ dags/main.py
@@ -1,8 +1,11 @@
 from airflow import DAG
 import pendulum
 from datetime import datetime, timedelta
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+
 from api.video_stats import get_playlist_id, get_video_ids, extract_video_data, save_to_json
 from datawarehouse.dwh import staging_table, core_table
+from dataquality.soda import yt_elt_data_quality
 
 # Define the local timezone
 local_tz = pendulum.timezone("Europe/London")
@@ -22,13 +25,18 @@ default_args = {
     # 'end_date': datetime(2030, 12, 31, tzinfo=local_tz),
 }
 
+# variables
+staging_schema="staging"
+core_schema="core"
+
+# DAG: 1: produce_json
 with DAG(
     dag_id='produce_json',
     default_args=default_args,
     description='DAG to produce JSON file with rawdata',
     schedule='0 14 * * *',
     catchup=False
-) as dag:
+) as dag_produce:
 
     #Define tasks
     playlist_id=get_playlist_id()
@@ -36,22 +44,48 @@ with DAG(
     extract_data=extract_video_data(video_ids)
     save_to_json_task=save_to_json(extract_data)
 
+    trigger_update_db = TriggerDagRunOperator(
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db",
+    )
+
     #Define dependencies
 
-    playlist_id >> video_ids >> extract_data >> save_to_json_task
+    playlist_id >> video_ids >> extract_data >> save_to_json_task >> trigger_update_db
 
+# DAG 2: update_db
 with DAG(
     dag_id='update_db',
     default_args=default_args,
     description='DAG to process JSON file and insert data into both staging and core schemas',
-    schedule='0 15 * * *',
+    schedule=None,
     catchup=False
-) as dag:
+) as dag_update:
 
     #Define tasks
     update_staging=staging_table()
     update_core=core_table()
 
-    #Define dependencies
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id="trigger_data_quality",
+        trigger_dag_id="data_quality",
+    )
 
-    update_staging >> update_core
+    #Define dependencies
+    update_staging >> update_core >> trigger_data_quality
+
+# DAG 3: data_quality
+with DAG(
+    dag_id='data_quality',
+    default_args=default_args,
+    description='DAG to check the data quality on both schemas of the db',
+    schedule=None,
+    catchup=False
+) as dag_quality:
+
+    #Define tasks
+    soda_validate_staging = yt_elt_data_quality(staging_schema)
+    soda_validate_core = yt_elt_data_quality(core_schema)
+
+    #Define dependencies
+    soda_validate_staging >> soda_validate_core
---
+++ tests/integration_test.py
@@ -0,0 +1,32 @@
+import requests
+import pytest
+import psycopg2
+
+def test_youtube_api_response(airflow_variable):
+    API_KEY = airflow_variable("api_key")
+    CHANNEL_HANDLE = airflow_variable("channel_handle")
+
+    url=f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
+
+    try:
+        response = requests.get(url)
+        assert response.status_code==200
+    except:
+        pytest.fail(f"Request to get youtube api failed: {e}")
+
+def test_real_postgres_connection(real_postgres_connection):
+    cursor = None
+
+    try:
+        cursor = real_postgres_connection.cursor()
+        cursor.execute("SELECT 1;")
+        result = cursor.fetchone()
+
+        assert result[0] == 1
+    
+    except psycopg2.Error as e:
+        pytest.fail(f"Database query failed: {e}")
+
+    finally:
+        if cursor is not None:
+            cursor.close()
